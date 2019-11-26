d77f958cb42c77db48186756043213f894f812e2
jhLee <kim0lil@naver.com>
2019-11-27 00:25:29 +0900

    2019-11-27

--- resources/SpringCore/src/aop/expression/Advice.java
+++ resources/SpringCore/src/aop/expression/Advice.java
@@ -1,6 +1,6 @@
 package aop.expression;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -10,32 +10,32 @@ import org.springframework.stereotype.Component;
 public class Advice {
 
 	@Before("aop.expression.PointCut.method_1()")
-	public void boforeAdvice_1() {
+	public void boforeAdvice_1(JoinPoint joinPoint) {
 		System.out.println("<-- boforeAdvice_1");
 	}
 	
 	@Before("aop.expression.PointCut.method_2()")
-	public void boforeAdvice_2() {
+	public void boforeAdvice_2(JoinPoint joinPoint) {
 		System.out.println("<-- boforeAdvice_2");
 	}
 	
 	@Before("aop.expression.PointCut.type_1()")
-	public void boforeAdvice_3() {
+	public void boforeAdvice_3(JoinPoint joinPoint) {
 		System.out.println("<-- boforeAdvice_3");
 	}
 	
 	@Before("aop.expression.PointCut.type_2()")
-	public void boforeAdvice_4() {
+	public void boforeAdvice_4(JoinPoint joinPoint) {
 		System.out.println("<-- boforeAdvice_4");
 	}
 	
 	@Before("aop.expression.PointCut.mix_1()")
-	public void boforeAdvice_5() {
+	public void boforeAdvice_5(JoinPoint joinPoint) {
 		System.out.println("<-- boforeAdvice_5");
 	}
 	
 	@Before("aop.expression.PointCut.mix_2()")
-	public void boforeAdvice_6() {
+	public void boforeAdvice_6(JoinPoint joinPoint) {
 		System.out.println("<-- boforeAdvice_6");
 	}
 	
--- resources/SpringCore/src/aop/package-info.java
+++ resources/SpringCore/src/aop/package-info.java
@@ -14,16 +14,53 @@
  *      +-- Advice
  *      |     |
  *      |     +-- @Before : 메서드가 실행 되기 전 처리
+ *      |     |       |
+ *      |     |       \-- JoinPoint : 조인포인트 정보      
  *      |     |
  *      |     +-- @After : 메서드가 실행 된 다음 처리
+ *      |     |       |
+ *      |     |       \-- JoinPoint : 조인포인트 정보
  *      |     |
  *      |     +-- @AfterReturning : 메서드 결과값이 반환 된 다음 처리
+ *      |     |       |
+ *      |     |       \-- JoinPoint : 조인포인트 정보
  *      |     |
  *      |     +-- @AfterThrowing : 메서드가 에러가 발생한 다음 처리
+ *      |     |       |
+ *      |     |       \-- JoinPoint : 조인포인트 정보
  *      |     |
- *      |     +-- @Around : 메서드의 실행을 메서드에게 위임
+ *      |     \-- @Around : 메서드의 실행을 메서드에게 위임
+ *      |             |
+ *      |             \-- PreceedingJoinPoint : 조인포인트 정보
  *      |
+ *      +-- Pointcut
+ *      |     |
+ *      |     \-- @Pointcut : 조인 포인트의 포인트컷을 정의하는 에너테이션
+ *      |             |
+ *      |             \-- String : Expression을 등록하여 외부에서도 참조 가능
  *      |
- *      
+ *      +-- Expression
+ *      |     |
+ *      |     +-- MethodSigniture : 메소드의 반환타입, (타입 + 메소드명), 파라미터 정보로 매칭
+ *      |     |        |
+ *      |     |        \-- String : [ execution(반환값 타입.메소드명(인자타입)) ]
+ *      |     |
+ *      |     \-- TypeSigniture : 타입명으로 매칭
+ *      |              |
+ *      |              \-- String : [ within(타입) ]
+ *      |
+ *      \-- Introduction
+ *            |
+ *            \-- @DeclareParents
+ *                    |
+ *                    +--value
+ *                    |    |
+ *                    |    \-- String : 끌어들일 객체이 타입
+ *                    |
+ *                    \-- defaultImpl
+ *                         |
+ *                         \-- Class : 상속받을 클래스
  */
 package aop;
+
+import org.aspectj.lang.annotation.DeclareParents;
